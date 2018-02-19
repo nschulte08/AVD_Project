008c6427e5657f25ef2465a310012dcaa174fe00
calle <calle@10.182.21.158>
2018-02-19 13:54:01 -0600

    Added the Equations that Ben found

--- StabilityDerivativeCode.m
+++ StabilityDerivativeCode.m
@@ -1,18 +1,24 @@
-%% Geometry Locations======================================================
-xbar_cg  =
-xbar_ACw = 
-%% Control Surface Deflections=============================================
-Dela     = 0.5*(Dela_l-Dela_r);
-%Dele    = look into flying wing elevators
-Delr     = 
-Cm_ih    = 0                                                     %since OFW
-Ih       = 0                                                     %since OFW
-%% Pitching Moment=========================================================
-Cm_0     = Cm_ACw+(CL_0w*(xbar_cg-xbar_ACw));
-Cm_aoa   = CL_aoaw*(xbar_cg-xbar_ACw));
-%Cm_dele = look into flying wing elevators
-Cm       = Cm_0+(Cm_aoa*AOA)+(Cm_dele*Dele)+(Cm_ih*Ih)
-%% Rolling Moment==========================================================
-Cl       = Cl_0+(Cl_beta*BETA)+(Cl_dela*Dela)+(Cl_delr*Delr)
-%% Yawing Moment===========================================================
-Cn       = Cn_0+(Cn_beta*BETA)+(Cn_dela*Dela)+(Cn_delr*Delr)
+%% StabilityDerivatives====================================================
+%WingGeometry==============================================================
+TR          =  
+c_r         = 
+c_bar       = ((2/3)*c_r*((1+TR+TR^2)/(1+TR)));
+z           = 
+z_t         = 
+tau         =  
+%Longitudinal==============================================================
+N           = (L*cos(AOA))+(D*sin(AOA));
+C           = (D*cos(AOA))-(L*sin(AOA));
+Cm_cg       = (-CL*(x_wing/c_bar))+(CD*(z/c_bar))+Cm_acwing+((T*z_t)/(q*S*tau));
+Cm_alpha    = ((-x_wing/c_bar)*Cl_alphaw);
+%Lateral===================================================================
+Cl_betawing = Cl_betabasic+Cl_betaD+Cl_betaLam
+Cl_betaLam  = -0.25*CL_alpha*Lambda*((2+(1+2*TR))/(3*(1+TR)));
+Cl_betaVT   = (-CL_alphaVT*(S_VT/S_ref)*(z_v/b)*((1+(dalpha/dbeta))*(q_VT/q)));
+Cl_beta     = Cl_betawing+Cl_betaVT;
+%Directional===============================================================
+N           = l_VT*L_VT+N_power+N_wing;
+Cn          = N/(q*S_ref*b);
+Cn_betawing = (CL^2*((1/4*pi*AR)-(tan(sweep)/pi*AR*(AR+4*cos(sweep)))*(cos(sweep)-(AR/2)-(AR^2/8*cos(sweep))+((6*x/c_bar)*(sin(sweep)/AR))));
+Cn_betaVT   = (V_VT*CL_alphaVT*((1+(dalpha/dbeta))*(q_VT/q)));
+Cn_beta     = Cn_betawing+Cn_betaVT;
