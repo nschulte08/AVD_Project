929eea34a198816b20e7e7efc0aaa0b03b65305c
blaberwack <35579793+blaberwack@users.noreply.github.com>
2018-03-03 12:24:46 -0600

    Create new_file_test_bollocks.m
    this is a test to see if Shawn can upload files in a new way

---
+++ new_file_test_bollocks.m
@@ -0,0 +1,13 @@
+% this is a test!!!
+
+function [outputs] = new_file_test_bollocks(inputs)
+
+
+outputs = inputs;
+
+fprintf('\n\n input = %g \n\n', inputs);
+fprintf('\n\n output = %g \n\n', outputs);
+fprintf('\n\n the output is the same as the input... \n\n');
+fprintf('\n\n wtf is the point!!?? \n\n ??? \n\n i dont know! \n\n');
+
+end
