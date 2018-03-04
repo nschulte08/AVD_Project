9da0f64e8ae14f129cc6b2654f636c35f306558a
shootingQuasar1 <callen1921@gmail.com>
2018-03-03 21:42:10 -0600

    Began adjusting the code for the plots of  the rolling and yawing moments

--- SC_RollingMoment.m
+++ SC_RollingMoment.m
@@ -35,24 +35,32 @@ Cl_betaLam  = -0.25*Cl_aoa*Lambda*((2+(1+2*TR))/(3*(1+TR))); %Nicolai 21.12
 Cl_betawing = Cl_betabasic+Cl_betaD+Cl_betaLam %Nicolai 21.11
 Cl_betavt   = (-Cl_aoavt*(S_vt/S_ref)*(z_v/b)*(vwt)); %Nicolai 21.14
 Cl_beta     = Cl_betawing+Cl_betavt; %Nicolai 21.10
+Cl_0        = 
 %% RollControlSurfaceSizing================================================
-ba_frac   = input('Please input the Aileron to wing span ratio:');
-ca_frac   = input('Please input the Aileron to chord length ratio:'); 
-tau_a     = ((1.5278*ca_frac^3)-(2.7083*ca_frac^2)+(2.2139*ca_frac)+0.0543);
-y_i       = input('Please input the Aileron inboard y-position:');
-y_o       = input('Please input the Aileron outboard y-position:');
-in1       = (((y_o^2*(4*(TR-1)*y_o+3*b))-((4*(TR-1)*y_i^3)-(3*b*y_i^2)))/(6*b));
-Cl_dela   = ((2*Cl_aoaw*tau_a*c_r)/(S*b))*(in1);
-dela_max  = input('Please input the Maximum Aileron Deflection Angle [degrees]: ');
-Cl        = Cl_dela*dela_max;
-L_A       = 2*delL*y_a;
-P_ss      = sqrt((2*L_A)/(rho*(S_w+S_vt)*C_Dr*(y_D^3)));
-phi1      = ((I_xx)/(rho*(y_D^3)*(S_w+S_vt)*C_Dr))*(log(P_ss^2));
-P_dot     = (P_ss^2)/(2*phi1);
+ba_frac     = input('Please input the Aileron to wing span ratio:');
+ca_frac     = input('Please input the Aileron to chord length ratio:'); 
+tau_a       = ((1.5278*ca_frac^3)-(2.7083*ca_frac^2)+(2.2139*ca_frac)+0.0543);
+y_i         = input('Please input the Aileron inboard y-position:');
+y_o         = input('Please input the Aileron outboard y-position:');
+in1         = (((y_o^2*(4*(TR-1)*y_o+3*b))-((4*(TR-1)*y_i^3)-(3*b*y_i^2)))/(6*b));
+Cl_dela     = ((2*Cl_aoaw*tau_a*c_r)/(S*b))*(in1);
+dela_max    = input('Please input the Maximum Aileron Deflection Angle [degrees]: ');
+Cl          = Cl_dela*dela_max;
+L_A         = 2*delL*y_a;
+P_ss        = sqrt((2*L_A)/(rho*(S_w+S_vt)*C_Dr*(y_D^3)));
+phi1        = ((I_xx)/(rho*(y_D^3)*(S_w+S_vt)*C_Dr))*(log(P_ss^2));
+P_dot       = (P_ss^2)/(2*phi1);
 if phi1 > phi_req
-t2        = sqrt((2*phi_des)/(P_dot));
+t2          = sqrt((2*phi_des)/(P_dot));
     else  phi1 < phi_req
-t2        = sqrt((2*phi1)/(P_dot))+((phi2-phi1)/(P_ss));
+t2          = sqrt((2*phi1)/(P_dot))+((phi2-phi1)/(P_ss));
 end
 %% Plots===================================================================
-    
+dela = -25:5:25;
+beta = -4:2:24; 
+    for n = 1:length(dele)
+        for j = 1:length(aoa)
+            BETA(j) = (beta(j)*(pi/180));
+            Cl(n,j) = (Cl_0+(Cl_beta*BETA(j))+(Cl_dela*dela(n))+(Cl_dela*delr));
+        end
+    end
--- SC_YawingMoment.m
+++ SC_YawingMoment.m
@@ -32,27 +32,36 @@ vwt         = (0.724+((3.06*(S_vtpr/S_ref))/(1+cos(sweep)))+(0.4*(z_w/d))+(0.009
 Cn_betawing = (Cl^2*((1/4*pi*AR)-(tan(sweep)/pi*AR*(AR+4*cos(sweep)))*(cos(sweep)-(AR/2)-(AR^2/8*cos(sweep))+((6*x/c_bar)*(sin(sweep)/AR))))); %Nicolai 21.22
 Cn_betavt   = (V_vt*Cl_aoavt*(vwt)); %Nicolai 21.21
 Cn_beta     = Cn_betawing+Cn_betavt; %Nicolai 21.20
+Cn_0        = 
 %% YawControlSurfaceSizing=================================================
     %% Asymmetric Thrust===================================================
-br_frac   = 1
-delr_max  = 30;
-V_conmin  = 16 
-N_A       = -T_L*y_T;
-Cn_delr   = N_A/(q*S*b*delr_max);
-tau_r     = (Cn_delr*b_vt/(-CL_aoav*V_v*dynpres_v*br_frac));
-cr_frac    = ((1.1873*(tau_r^2))-(0.1602*tau_r)+0.0678);
+br_frac     = 1
+delr_max    = 30;
+V_conmin    = 16 
+N_A         = -T_L*y_T;
+Cn_delr     = N_A/(q*S*b*delr_max);
+tau_r       = (Cn_delr*b_vt/(-CL_aoav*V_v*dynpres_v*br_frac));
+cr_frac     = ((1.1873*(tau_r^2))-(0.1602*tau_r)+0.0678);
     %% Cross-wind Landing==================================================
-V_v       = ((l_vt*S_v)/(b*S));
-V_w       = 28
-V_t       = sqrt((U1^2)+(V_w^2));
-S_s       = 84
-F_w       = 0.5*rho*(V_w^2)*S_s*Cd_y;
-br_frac    = 1
-cr_frac    = 1
-beta      = atan(V_w/U1);
-Cn_beta   = K_f1*Cl_aoav*(vwt)*((l_vt*S_v)/S)
-Cy_beta   = K_f2*Cl_aoav*(vwt)*((S_v)/S)
-tau_r     = ((1.5278*cr_frac^3)-(2.7083*cr_frac^2)+(2.2139*cr_frac)+0.0543);
-Cy_delr   = Cl_aoav*dynpres_v*tau_r*br_frac*(S_v/S);
-Cn_delr   = -Cl_aoav*V_v*dynpres_v*tau_r*br_frac; 
+V_v         = ((l_vt*S_v)/(b*S));
+V_w         = 28
+V_t         = sqrt((U1^2)+(V_w^2));
+S_s         = 84
+F_w         = 0.5*rho*(V_w^2)*S_s*Cd_y;
+br_frac     = 1
+cr_frac     = 1
+beta        = atan(V_w/U1);
+Cn_beta     = K_f1*Cl_aoav*(vwt)*((l_vt*S_v)/S)
+Cy_beta     = K_f2*Cl_aoav*(vwt)*((S_v)/S)
+tau_r       = ((1.5278*cr_frac^3)-(2.7083*cr_frac^2)+(2.2139*cr_frac)+0.0543);
+Cy_delr     = Cl_aoav*dynpres_v*tau_r*br_frac*(S_v/S);
+Cn_delr     = -Cl_aoav*V_v*dynpres_v*tau_r*br_frac; 
 %% Plots===================================================================
+delr = -25:5:25;
+beta = -4:2:24; 
+    for n = 1:length(dele)
+        for j = 1:length(aoa)
+            BETA(j) = (beta(j)*(pi/180));
+            Cn(n,j) = (Cn_0+(Cn_beta*BETA(j))+(Cn_delr*delr(n))+(Cl_dela*dela));
+        end
+    end
