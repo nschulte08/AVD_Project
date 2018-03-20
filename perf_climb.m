2853bcfbbd1d6cb4225922a82348e4aea9f17e3d
blaberwack <35579793+blaberwack@users.noreply.github.com>
2018-03-20 02:54:37 -0500

    Update perf_climb.m

--- perf_climb.m
+++ perf_climb.m
@@ -1,32 +1,30 @@
-function [ROC, gamma_climb, S_climb, dt_climb] = perf_climb(CD_climb, V_stall, V_sub, rho_TO, T_max, W_climb, alt_climb, V_climb)
-%This function outputs the performance characteristics of the OFW during
-%the climb phase
-
-%Inputs:
-%CD_climb:          Climb drag coefficient
-%V_stall:           Stall velocity (m/s)
-%V_sub_R:           Subsonic velocity (m/s)
-%rho_TO:            Takeoff density (kg/m^3)
-%T_max:             Sea level max thrust (N)
-%W_climb:           Climb weight from wt fraction analysis
-
-%Outputs:
-%ROC:               Rate of climb (m/s)
-%gamma_climb:       Climb angle (deg)
-%S_climb_sub:       Distance covered when climbing to subsonic cruise altitude (m)
-%S_climb_super:     Distance covered when climbing to supersonic cruise altitude (m)
-%dt_climb_sub:      Time of flight during climb when climbing to subsonic cruise altitude
-%dt_climb_super:    TOF when climbing to supersonic cruise altitude
-
-%Local Inputs:
+%{
+This function outputs the performance characteristics during the climb phase
+---------------------------------------------------------------------------
+Inputs:
+CL_climb:          Climb lift coefficient
+CD_climb:          Climb drag coefficient
+V_stall:           Stall velocity (m/s)
+V_sub_cr:           Subsonic cruise velocity (m/s)
+rho_TO:            Takeoff air density (kg/m^3)
+T_max:             Sea level max thrust (N)
+W_climb:           Climb weight from wt fraction analysis
+---------------------------------------------------------------------------
+Outputs:
+ROC:               Rate of climb (m/s)
+gamma_climb:       Climb angle (deg)
+S_climb:           Distance covered during clim (m)
+dt_climb:          Time to climb
+===========================================================================
+%}
+function [ROC, gamma_climb, S_climb, dt_climb] = perf_climb(CL_climb, CD_climb, V_stall, V_sub_cr, rho_TO, T_max, W_climb, alt_climb, Sref)
 
 
 %% ========================================================================
 % Maximum ROC:
 %--------------------------------------------------------------------------
-
-V = linspace(V_stall,V_sub)';                    % [m/s] veloctiy vector
-D = 0.5 * rho_TO * V.^2 * CD_climb * Sref;         % [N] drag
+V = linspace(V_stall,V_sub_cr)';                   % [m/s] veloctiy vector
+D = 0.5*rho_TO*V.^2*CD_climb*Sref;                 % [N] drag
 ROC = V.*(T_max - D)./W_climb;                     % [m/s] rate of climb
 [max_ROC, ind] = max(ROC);                         % [m/s] maximum rate of climb
 
@@ -34,11 +32,13 @@ gamma_climb_max = asind((T_max - D(ind))/W_climb); % [deg] corresponding climb a
 gamma_climb = floor(gamma_climb_max);              % [deg] climb angle
 %gamma_climb = 15; % [deg]
 
+%{
 figure('Name','Max ROC vs Velocity at Sea Level','NumberTitle','off');
 plot(V,ROC,'k', 'LineWidth',2);
 title('Maximum Rate of Climb','FontSize',18);
 xlabel('Airspeed [m/s]','FontSize',12);
 ylabel('ROC [m/s]','FontSize',12);
+%}
 % =========================================================================
 fprintf('\n\n ============================ Climb Results  ============================= \n');
 fprintf('\n Sea level Rate of Climb: \n');
@@ -48,12 +48,12 @@ fprintf('\n Corresponding Climb Velocity:  V = %g [m/s] = %g [ft/s]', V(ind), V(
 fprintf('\n\n -------------------------------------------------------------------- ');
 %--------------------------------------------------------------------------
 % Normal Climb:
-%V_climb = sqrt((2/(rho_TO*CL_climb))*(W_climb/Sref)*cosd(gamma_climb)); % [Raymer eq. 17.38]
+V_climb = sqrt((2/(rho_TO*CL_climb))*(W_climb/Sref)*cosd(gamma_climb)); % [Raymer eq. 17.38]
 
 % ROC_norm = V_climb*sind(gamma_climb); % [m/s]
 
-S_climb = alt_climb/tand(gamma_climb);     % [m] range covered to climb to subsonic cruise altitude
-dt_climb = S_climb/(V_climb*cosd(gamma_climb));     % [s] time to climb to subsonic cruise altitude
+S_climb = alt_climb/tand(gamma_climb);          % [m] range covered to climb to subsonic cruise altitude
+dt_climb = S_climb/(V_climb*cosd(gamma_climb)); % [s] time to climb to subsonic cruise altitude
 
 end
 
