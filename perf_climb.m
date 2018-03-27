c0cb2e441fe00f978eb5df58c236014fbb6d192b
blaberwack <35579793+blaberwack@users.noreply.github.com>
2018-03-27 18:46:41 -0500

    Update perf_climb.m
    	completely rewrote... dont really need MAX sea level ROC
    	assumed a climb angle and performed simple performance analysis...
    	new climb function (updated perf_climb.m)
    	also incorporates wing sweep (if any) during climb phase

--- perf_climb.m
+++ perf_climb.m
@@ -2,13 +2,11 @@
 This function outputs the performance characteristics during the climb phase
 ---------------------------------------------------------------------------
 Inputs:
-CL_climb:          Climb lift coefficient
-CD_climb:          Climb drag coefficient
-V_stall:           Stall velocity (m/s)
-V_sub_cr:           Subsonic cruise velocity (m/s)
-rho_TO:            Takeoff air density (kg/m^3)
-T_max:             Sea level max thrust (N)
-W_climb:           Climb weight from wt fraction analysis
+alt_climb:         top of climb altitude [m]
+CL_max:            max lift coefficient
+W_climb:           Climb weight from [N]
+Sref:              referrence area [m^2]
+b_unswept:         unswept span [m]
 ---------------------------------------------------------------------------
 Outputs:
 ROC:               Rate of climb (m/s)
@@ -17,43 +15,46 @@ S_climb:           Distance covered during clim (m)
 dt_climb:          Time to climb
 ===========================================================================
 %}
-function [ROC, gamma_climb, S_climb, dt_climb] = perf_climb(CL_climb, CD_climb, V_stall, V_sub_cr, rho_TO, T_max, W_climb, alt_climb, Sref)
-
-
-%% ========================================================================
-% Maximum ROC:
+function [ROC, gamma_climb, S_climb, dt_climb] = perf_climb(alt_climb, CL_max, W_climb, Sref, b_unswept)
 %--------------------------------------------------------------------------
-V = linspace(V_stall,V_sub_cr)';                   % [m/s] veloctiy vector
-D = 0.5*rho_TO*V.^2*CD_climb*Sref;                 % [N] drag
-ROC = V.*(T_max - D)./W_climb;                     % [m/s] rate of climb
-[max_ROC, ind] = max(ROC);                         % [m/s] maximum rate of climb
-
-gamma_climb_max = asind((T_max - D(ind))/W_climb); % [deg] corresponding climb angle
-gamma_climb = floor(gamma_climb_max);              % [deg] climb angle
-%gamma_climb = 15; % [deg]
-
-%{
-figure('Name','Max ROC vs Velocity at Sea Level','NumberTitle','off');
-plot(V,ROC,'k', 'LineWidth',2);
-title('Maximum Rate of Climb','FontSize',18);
-xlabel('Airspeed [m/s]','FontSize',12);
-ylabel('ROC [m/s]','FontSize',12);
-%}
-% =========================================================================
-fprintf('\n\n ============================ Climb Results  ============================= \n');
-fprintf('\n Sea level Rate of Climb: \n');
-fprintf('\n Maximum Rate of Climb:         ROC = %g [m/s] = %g [ft/s]', max_ROC, max_ROC*3.2808399);
-fprintf('\n Corresponding Climb Angle:     gamma = %g [deg] ', gamma_climb_max);
-fprintf('\n Corresponding Climb Velocity:  V = %g [m/s] = %g [ft/s]', V(ind), V(ind)*3.2808399);
-fprintf('\n\n -------------------------------------------------------------------- ');
+[~, ~, ~, rho_SL, son_SL, ~, ~, ~, ~, ~] = ATMO(0, 'm');           % sea level
+[~, ~, ~, rho_top, son_top, ~, ~, ~, ~, ~] = ATMO(alt_climb, 'm'); % top of climb
+rho_climb = 0.5*(rho_SL + rho_top);                                % everage air density [kg/m^3]
+son_climb = 0.5*(son_SL + son_top);                                % everage speed of sound [m/s]
 %--------------------------------------------------------------------------
-% Normal Climb:
-V_climb = sqrt((2/(rho_TO*CL_climb))*(W_climb/Sref)*cosd(gamma_climb)); % [Raymer eq. 17.38]
+CL_climb = CL_max/(1.25^2);	 % climb lift coefficient
+gamma_climb = 15;            % [deg] climb angle
 
-% ROC_norm = V_climb*sind(gamma_climb); % [m/s]
+V_stall = sqrt(2*W_climb/(rho_climb*Sref*CL_climb)); % [m/s]
+V_climb = sqrt((2/(rho_climb*CL_climb))*(W_climb/Sref)*cosd(gamma_climb)); % [Raymer eq. 17.38]
 
-S_climb = alt_climb/tand(gamma_climb);          % [m] range covered to climb to subsonic cruise altitude
-dt_climb = S_climb/(V_climb*cosd(gamma_climb)); % [s] time to climb to subsonic cruise altitude
-
+if V_climb <= V_stall
+    V_climb = 1.5*V_stall; % [m/s]
 end
 
+M_climb = V_climb/son_climb; % climb Mach number
+%--------------------------------------------------------------------------
+% Calculate sweep for climb:
+sweep_deg_climb = acosd(0.7/M_climb);
+if sweep_deg_climb > 70   % Limit the sweep angle to 70 ish degrees
+    sweep_deg_climb = 70;
+end
+b_swept_climb = b_unswept*cosd(sweep_deg_climb);  % Span at sweep angle [m]
+AR_climb = b_swept_climb^2/Sref;                  % Swept aspect ratio
+% ========================================================================
+ROC = V_climb*sind(gamma_climb);                % rate of climb [m/s]
+S_climb = alt_climb/tand(gamma_climb);          % [m] range covered to climb to subsonic cruise altitude
+dt_climb = S_climb/(V_climb*cosd(gamma_climb)); % [s] time to climb to subsonic cruise altitude
+%--------------------------------------------------------------------------
+fprintf('\n\n ============================ Climb Results  ============================= \n');
+fprintf('\n Climb sweep angle:              Gamma = %g [deg] ', sweep_deg_climb);
+fprintf('\n Climb aspect ratio:             AR_climb = %g [deg] ', AR_climb);
+fprintf('\n effective span during climb:    b_eff = %g [m] = %g [ft] ', b_swept_climb, b_swept_climb*3.2808399);
+fprintf('\n\n -------------------------------------------------------------------- ');
+fprintf('\n Rate of Climb:                  ROC = %g [m/s] = %g [ft/s]', ROC, ROC*3.2808399);
+fprintf('\n Climb Angle:                    gamma = %g [deg] ', gamma_climb);
+fprintf('\n Climb Velocity:                 V = %g [m/s] = %g [ft/s]', V_climb, V_climb*3.2808399);
+fprintf('\n\n -------------------------------------------------------------------- ');
+fprintf('\n Time to climb:                  TOF = %g [s] = %g [min]', dt_climb, dt_climb/60);
+fprintf('\n\n ========================================================================= \n');
+end
