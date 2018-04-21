6d63f8a4f13f4c4201e1e8fd9d197897f72c3d1f
blaberwack <35579793+blaberwack@users.noreply.github.com>
2018-04-20 21:54:41 -0500

    Update stability.m
    removed vertical tail sizing (this is now in an independent function).

--- stability.m
+++ stability.m
@@ -3,15 +3,16 @@
 Stability Derivatives function
 ===========================================================================
 INPUTS:
-h       = altitude [m]
-M       = Mach number
-b       = effective span [m]
-S_ref   = referrence area [m^2]
-AR      = effective aspect ratio
-Lambda  = sweep angle [deg]
-TR      = taper ratio
-CL      = lift coefficient
-SM      = static margin
+M           = Mach number
+AR          = effective aspect ratio
+sweep_deg   = sweep angle [deg]
+S_ref       = referrence area [m^2]
+b_eff       = effective span [m]
+TR          = taper ratio
+CL          = lift coefficient
+SM          = static margin
+S_VT        = total required vertical tail area [m^2]
+C_VT        = vertical tail area volume coefficient
 flight_phase = string for plot naming
 ===========================================================================
 OUTPUTS:
@@ -21,66 +22,30 @@ Cn_beta = directional stability derivative [1/rad]
 CM_de   = elevator control power [1/rad]
 Cl_da   = aileron control power [1/rad]
 Cn_dr   = rudder control power [1/rad]
-S_VT    = total required vertical tail area [m^2]
-l_VT    = distance (in x-direction) between cg location and 1/4 chord of VT mac [m]
 ===========================================================================
 %}
-function [CMa, Cl_beta, Cn_beta, CM_de, Cl_da, Cn_dr, S_VT, l_VT, vert_tail_plot] = stability(M, AR, Lambda, S_ref, b, TR, CL, SM, flight_phase)
+function [CMa, Cl_beta, Cn_beta, CM_de, Cl_da, Cn_dr] = stability(M, AR, sweep_deg, S_ref, b_eff, TR, CL, SM, S_VT, C_VT, flight_phase)
 %--------------------------------------------------------------------------
 % geometry:
 tmax = 2.3;         % Maximum Thickness
 tcmax = 0.16;       % T/c max; variable to iterate (this is internal to aero team... ow one else uses it)
 c_r = tmax/tcmax;   % [m] Root chord = max thickness / tcmax ratio
 c_bar = ((2/3)*c_r*((1+TR+TR^2)/(1+TR)));  % Nicolai Pg 580
+if M < 1
+    Beta = sqrt(1 - M^2);
+else
+    Beta = sqrt(M^2 - 1);
+end
 
-%% ========================================================================
-% vertical tail sizing:
-C_VT = 0.09;            % Nicolai table 11.8 (buisness aircraft)
-lVT_SVT = C_VT*b*S_ref; % [m^3] product of l_VT and S_VT (Nicolai p.286)
-%--------------------------------------------------------------------------
-% plot solutions:
-l_VT_plot = 1:0.1:b;           % [m] distance (in x-direction) between cg location and 1/4 chord of VT mac
-S_VT_plot = lVT_SVT./l_VT_plot; % [m^2] VT area
-
-vert_tail_plot = [l_VT_plot; S_VT_plot];
-%{
-figure_name = sprintf('Required Total Vertical Tail Area vs Distance of Vertical Tail mac to cg Location, for %s', flight_phase);
-figure('Name',figure_name,'NumberTitle','off','units','normalized','outerposition',[0 0 1 1]);
-plot(l_VT_plot,S_VT_plot, 'k', 'LineWidth',3);
-xlabel('l_V_T (m)'  ,'FontSize',18);
-ylabel('S_V_T (m^2)','FontSize',18);
-xlim([1,b]);
-title_string = sprintf('%s Required Total Vertical Tail Area vs Distance of Vertical Tail mac to cg Location',flight_phase);
-title(title_string,'FontSize',18);
-grid on
-fig_save('Figures', figure_name)
-%}
-%--------------------------------------------------------------------------
-% inital estimate:
-l_VT = (3/8)*b*cosd(Lambda); % this is a function of sweep
-S_VT = lVT_SVT/l_VT;
-CL_a_VT = 2*pi; % [1/rad] vertical tail lift curve slope (approximation for now)
-%{
-fprintf('\n\n ============================================================= \n');
-fprintf('\n %s Vertical tail size estimate:', flight_phase);
-fprintf('\n\n Total required vertical tail area:');
-fprintf('\n S_VT = %g [m^2]', S_VT);
-fprintf('\n\n Distance between cg location and 1/4 chord of VT mac:');
-fprintf('\n l_VT = %g [m]', l_VT);
-fprintf('\n\n ============================================================= \n');
-%}
 %% ========================================================================
 % Longitudinal stability:
 %--------------------------------------------------------------------------
 %CMa = -1.2;
 tcmax = 0.16;
 Cla = 1.8*pi*(1 + 0.8*tcmax); %Airfoil Cl_alpha, Saedray pg 179  
-Beta_sub = sqrt(abs(1-M^2));
-nu = Cla/(2*pi/Beta_sub);
-M_perp = 0.7; % perpendicular Mach #
-sweep_deg = acosd(M_perp/M);
+nu = Cla/(2*pi/Beta);
 sweep_rad = sweep_deg*pi/180; % Convert to radians for formulas
-CLa_sub = 2*pi*AR/(2+sqrt(4+((AR^2*Beta_sub^2)/nu^2)*(1+tan(sweep_rad)^2/Beta_sub^2))); 
+CLa_sub = 2*pi*AR/(2+sqrt(4+((AR^2*Beta^2)/nu^2)*(1+tan(sweep_rad)^2/Beta^2))); 
 CLa_super = 4/(sqrt(M^2-1))*(1-1/(2*AR*sqrt(M^2-1))); %Straight,tapered wings in supersonic flow
 if M < 1
     CLa = CLa_sub;
@@ -115,7 +80,7 @@ fprintf('\n\n ============================================================= \n')
 %--------------------------------------------------------------------------
 de_plot = -5:5:5;    % [deg] elevator deflection
 alpha_plot = -10:10; % [deg] side slip
-
+%{
 figure_name = sprintf('Longitudinal Stability, for %s', flight_phase);
 figure('Name',figure_name,'NumberTitle','off','units','normalized','outerposition',[0 0 1 1]);
 hold on
@@ -133,9 +98,12 @@ title(title_string,'FontSize',18);
 legend('\delta_e = -5 [deg]','\delta_e = 0 [deg]','\delta_e = 5 [deg]');
 grid on
 fig_save('Figures', figure_name)
+%}
 %% ========================================================================
 % Lateral stability:
 %--------------------------------------------------------------------------
+CL_a_VT = 2*pi; % approximate estimate
+
 % wing contribution:
 Cl_b_basic = -1/AR;  % completely fictional equation ---> need to find this ---> use DATCOM handbook?? (via Nicolai's suggestion)
 Cl_b_Lambda = -0.05; % due to sweep? ---> what equation is this?? fig. 21.10...?
@@ -144,20 +112,19 @@ Cl_b_wing = Cl_b_basic + Cl_b_Lambda + Cl_b_Gamma;
 
 % vertical tail contribution:
 z_v = 1; % [m] vertical distance between cg location and vertical tail ac (place holder)
-var_2115 = 0.724 + (3.06*(S_VT/S_ref))/(1 + cosd(Lambda)) + 0.009*AR; % Nicolai eq. 21.15
-Cl_b_VT = -CL_a_VT*var_2115*(S_VT/S_ref)*(z_v/b);
+var_2115 = 0.724 + (3.06*(S_VT/S_ref))/(1 + cosd(sweep_deg)) + 0.009*AR; % Nicolai eq. 21.15
+Cl_b_VT = -CL_a_VT*var_2115*(S_VT/S_ref)*(z_v/b_eff);
 
 Cl_beta = Cl_b_wing + Cl_b_VT; % lateral stability derivative
 
-beta = sqrt((1-M^2));
 CL_am = 0.2;
-k   = ((CL_am)*beta)/(2*pi);
+k   = ((CL_am)*Beta)/(2*pi);
 cl_dfrac = 0.625; % Roskam VI Figure 8.15
 cl_dth = 4.2; % Roskam VI Figure 8.14
 a_da = (cl_dfrac*cl_dth)/(0.2);
-Cprimel_d = (k/beta)*(0.3);
+Cprimel_d = (k/Beta)*(0.3);
 Cl_d = a_da*Cprimel_d;
-Cl_da = 2*Cl_d; % aileron control power ---> use Roskam
+Cl_da = 2*Cl_d; % aileron control power ---> Roskam
 %{
 fprintf('\n\n ============================================================= \n');
 fprintf('\n %s Lateral Stability Derivatives:', flight_phase);
@@ -171,7 +138,7 @@ fprintf('\n\n ============================================================= \n')
 %--------------------------------------------------------------------------
 da_plot = -5:5:5;   % [deg] rudder deflection
 beta_plot = -10:10; % [deg] side slip
-
+%{
 figure_name = sprintf('Lateral Stability, for %s', flight_phase);
 figure('Name',figure_name,'NumberTitle','off','units','normalized','outerposition',[0 0 1 1]);
 hold on
@@ -189,12 +156,13 @@ title(title_string,'FontSize',18);
 legend('\delta_a = -5 [deg]','\delta_a = 0 [deg]','\delta_a = 5 [deg]');
 grid on
 fig_save('Figures', figure_name)
+%}
 %% ========================================================================
 % Directional stability:
 %--------------------------------------------------------------------------
 % wing contribution:
 x = SM*c_bar; % [m] distance between cg location and wing ac
-Cn_b_wing = CL^2*(1/(4*pi*AR) - tand(Lambda)/(pi*AR*(AR + 4*cosd(Lambda)))*(cosd(Lambda) - AR/2 - AR^2/(8*cosd(Lambda)) + 6*x*sind(Lambda)/(c_bar*AR))); % Nicolai eq. 21.22
+Cn_b_wing = CL^2*(1/(4*pi*AR) - tand(sweep_deg)/(pi*AR*(AR + 4*cosd(sweep_deg)))*(cosd(sweep_deg) - AR/2 - AR^2/(8*cosd(sweep_deg)) + 6*x*sind(sweep_deg)/(c_bar*AR))); % Nicolai eq. 21.22
 
 % vertical tail contribution:
 Cn_b_VT = C_VT*CL_a_VT*var_2115; % Nicolai eq. 21.21
@@ -221,7 +189,7 @@ fprintf('\n\n ============================================================= \n')
 %--------------------------------------------------------------------------
 dr_plot = -5:5:5;   % [deg] rudder deflection
 beta_plot = -10:10; % [deg] side slip
-
+%{
 figure_name = sprintf('Directional Stability, for %s', flight_phase);
 figure('Name',figure_name,'NumberTitle','off','units','normalized','outerposition',[0 0 1 1]);
 hold on
@@ -239,5 +207,6 @@ title(title_string,'FontSize',18);
 legend('\delta_r = -5 [deg]','\delta_r = 0 [deg]','\delta_r = 5 [deg]'),%'\delta_r = 6 [deg]','\delta_r = 8 [deg]','\delta_r = 10 [deg]','Location', 'best');
 grid on
 fig_save('Figures', figure_name)
+%}
 %% ========================================================================
 end
